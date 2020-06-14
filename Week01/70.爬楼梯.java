65bde5173ea95e16edf446fafcb826b3d41f2845
DuanBinbin <talentduanbin@163.com>
2020-06-14 18:04:58 +0800

    添加其他的习题

---
+++ Week01/70.爬楼梯.java
@@ -0,0 +1,25 @@
+import jdk.nashorn.internal.ir.ReturnNode;
+
+/*
+ * @lc app=leetcode.cn id=70 lang=java
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int f1 = 1, f2 = 2, f3 = 3;
+        for (int i = 3; i <= n ; i++) {
+            f3 = f2 + f1;
+            f1 = f2;
+            f2 = f3;
+        }
+        return f3;
+    }
+}
+// @lc code=end
+
