238c7e9ccc59a5acfb57c8d77af3c1977191f1c6
DuanBinbin <talentduanbin@163.com>
2020-07-05 23:27:00 +0800

    提交第四周

---
+++ Week04/860.柠檬水找零.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=860 lang=java
+ *
+ * [860] 柠檬水找零
+ */
+
+// @lc code=start
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        if (bills.length == 0){
+            return false;
+        }
+
+        int five = 0, ten = 10;
+        for (int bill : bills) {
+            if (bill == 5){
+                five++;
+            } else if (bill == 10) {
+                if (five == 0) {
+                    return false;
+                }
+                five--;
+                ten++;
+            } else {
+                if (five > 0 && ten > 10) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
+// @lc code=end
+
