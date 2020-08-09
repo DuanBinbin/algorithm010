d7470bf7cc7b6ad80d910a38339b8d1ffee0b8f3
DuanBinbin <talentduanbin@163.com>
2020-08-09 23:59:57 +0800

    添加第10周作业题

---
+++ Week09/300.最长上升子序列.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=java
+ *
+ * [300] 最长上升子序列
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxanx = 1;
+        for (int i = 0; i < dp.length; i++) {
+            int maxval = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    maxval = Math.max(maxval, dp[j]);
+                }
+            }
+            dp[i] = maxval + 1;
+            maxanx = Math.max(maxanx, dp[i]);
+        }
+
+        return maxanx;
+    }
+}
+// @lc code=end
+
